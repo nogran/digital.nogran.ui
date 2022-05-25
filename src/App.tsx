a60b01acaa6ba54c366251456b5661650126fd0f
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-25 13:44:45 -0300

    nogran create website

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './pages/Home/Home';
+import './App.css';
+
+function App() {
+  return (
+    <Home />
+   );
+}
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <img src="https://i.ibb.co/rdPFk39/NOGRAN-logo.png" alt="logo black" className="logo-black"/>
+        </>
+    )
+}
+
+export default Home
