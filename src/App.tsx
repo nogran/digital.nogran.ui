bd9f281dff0b9efcf9f64a82cc18a6dbc90e9f54
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-26 01:19:31 -0300

    nogran

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import Header from './pages/Header/Header';
 import './App.css';
 
 function App() {
   return (
+    <div>
     <Home />
+    <Header />
+    </div>
    );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Paper, Box, ButtonGroup, Button, Grid } from '@material-ui/core';
 import './Home.css';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Home(){
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+function Home() {
+    const classes = useStyles();
+
     return (
         <>
-            <img src="https://i.ibb.co/rdPFk39/NOGRAN-logo.png" alt="logo black" className="logo-black"/>
+            <Grid container>
+                <Grid item xs={3} >
+                    <Box p={2}>
+                        <img src={require('../../assets/logo-nogran-black.png')} alt="logo black" className="logo-black" />
+                    </Box>
+                </Grid>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={5}>
+                    <Box display="flex" justifyContent="center">
+                        <div className={classes.root}>
+                            <Button variant="outlined">Home</Button>
+                            <Button variant="outlined">Empresa</Button>
+                            <Button variant="outlined">Serviços</Button>
+                            <Button variant="outlined">Contato</Button>
+                        </div>
+                    </Box>
+                </Grid>
+            </Grid>
         </>
-    )
+    );
 }
 
 export default Home
