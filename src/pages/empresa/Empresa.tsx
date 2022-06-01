daabd07ae2d2a7bb6a9d385d8b630e9590b34717
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-01 19:24:50 -0300

    empresa page developing

--- src/pages/empresa/Empresa.tsx
+++ src/pages/empresa/Empresa.tsx
@@ -23,8 +23,29 @@ export const Empresa = () => {
     return (
         <>
             <Navbar acao={ativaCor} />
-            <Grid container direction="row">
+            <Grid container direction="row" justifyContent='center' alignItems='center' >
 
+                {/* PAGE 1 */}
+                <Grid item className="empresa1" xs={4} style={{ height: '40vh' }}>
+                </Grid>
+                <Grid item className="empresa2" xs={8} style={{ height: '40vh' }} >
+                </Grid>
+
+                <Grid item xs={6} style={{ height: '80vh', backgroundColor: 'blue' }}>
+                    <Typography align="center" variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}> DIGITAL NOGRAN</Typography>
+                </Grid>
+
+                <Grid item xs={6} style={{ height: '80vh', backgroundColor: 'yellow' }}>
+                    <Typography></Typography>
+                </Grid>
+
+                {/* PAGE 2 */}
+                <Grid item className="empresa1" xs={12} style={{ height: '40vh' }}>
+                </Grid>
+
+                {/* PAGE 3 */}
+                <Grid item xs={12} style={{ height: '80vh' }}>
+                </Grid>
             </Grid>
         </>
     );
