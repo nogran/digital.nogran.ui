5b4fc165461fb44f0b5f118f06af71a2b2857920
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-01 22:19:11 -0300

    update servicos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/statics/header/Header';
 import Footer from './components/statics/footer/Footer';
-import { Contato } from './pages/contato/Contato';
 import { Home } from './pages/Home/Home';
 import { Empresa } from './pages/empresa/Empresa';
+import { Servicos } from './pages/servicos/Servicos';
+import { Contato } from './pages/contato/Contato';
 
 export const App = () => {
 
@@ -16,12 +16,14 @@ export const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/empresa" element={<Empresa />} />
-            {/* <Route path="/servicos" element={<Servicos />} /> */}
+            <Route path="/servicos" element={<Servicos />} />
             <Route path="/contato" element={<Contato />} />
           </Routes>
         </div>
 
-        <Footer />
+        <div style={{ minHeight: '30vh' }}>
+          <Footer />
+        </div>
       </Router>
     </>
   );
---
+++ src/pages/servicos/Servicos.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Box, Button, Grid, TextField, Typography, withStyles } from "@material-ui/core";
+import './Servicos.css';
+import Navbar from "../../components/statics/navbar/Navbar";
+
+const StyledButton = withStyles({
+    root: {
+        backgroundColor: '#656565',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: 'black',
+            // color: 'black',
+        },
+    }
+})(Button);
+
+export const Servicos = () => {
+
+    const [ativaCor, setAtivaCor] = useState(true);
+
+    useEffect(function () {
+        function posicaoScroll() {
+            if (window.scrollY > 40) {
+                setAtivaCor(true);
+            } else {
+                setAtivaCor(true);
+            }
+        }
+        window.addEventListener('scroll', posicaoScroll);
+        window.scrollTo(0, 0)
+    }, []);
+
+    return (
+        <>
+            <Navbar acao={ativaCor} />
+            <Grid container direction="row" justifyContent='center' alignItems='center'>
+            </Grid>
+        </>
+    )
+}
