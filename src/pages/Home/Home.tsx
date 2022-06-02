7a1463bfa8f81de3522e7ac9f8b02082f31c301f
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-01 22:19:19 -0300

    update home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -37,86 +37,86 @@ export const Home = () => {
         <>
             <Navbar acao={ativaCor} />
             <Swiper />
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid container direction="row" justifyContent="center" alignItems="center" spacing={0}>
 
                 {/* PAGE 1 */}
-                <Grid item xs={6} alignItems="center" >
-                    <Box justifyContent="center" alignItems="center">
-
-                        <Box marginLeft={20}>
-                            <Typography variant='h3'>Soluções</Typography>
-                        </Box>
-                        <Box marginLeft={20}>
-                            <Typography align="left" variant='h3'>Tecnológicas</Typography>
-                        </Box>
-                        <Box marginLeft={20} marginTop={2}>
-                            <Typography align="left" variant='h5'>Equipe preparada e experiente</Typography>
-                        </Box>
-                        <Box marginLeft={20}>
-                            <Typography align="left" variant='h5'>para diagnosticar sua necessidade</Typography>
-                        </Box>
-                        <Box marginLeft={20}>
-                            <Typography align="left" variant='h5'>e oferecer a melhor solução para</Typography>
-                        </Box>
-                        <Box marginLeft={20}>
-                            <Typography align="left" variant='h5'>o seu negócio.</Typography>
-                        </Box>
+                <Grid item xs={6} style={{ height: '60vh' }} alignItems="center">
+                    <Box paddingLeft={20} paddingY={20}>
+                        <Typography variant='h4'>Soluções</Typography>
+                        <Typography variant='h4'>Tecnológicas</Typography>
+                        <Typography variant='h6'>Nossa equipe é especializada em diagnosticar a sua necessidade e desenvolver a melhor solução para o seu negócio. Trabalhamos com grandes empresas até startups, sempre entregando aplicações web robustas para Sistemas Customizados.</Typography>
                     </Box>
                 </Grid>
 
-                <Grid item style={{ height: '80vh' }} xs={6} alignItems='center'>
-                    <Box marginTop={17} display='flex' justifyContent='center' alignItems='center'>
-                        <img width="45%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="" />
+                <Grid item style={{ height: '60vh' }} xs={6} alignItems='center'>
+                    <Box sx={{ p: 20 }} display='flex' justifyContent='center' alignItems='center'>
+                        <img width="80%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="" />
                     </Box>
                 </Grid>
 
                 {/* PAGE 2 */}
                 <Grid item xs={6} style={{ height: '80vh' }}>
-                    <Box marginTop={8} display='flex' justifyContent='center' alignItems='center'>
+                    <Box display='flex' justifyContent='center' alignItems='center' marginTop={10}>
                         <img width="35%" src={require("../../assets/logo/logo-nogran-vertical.png")} alt="" />
                     </Box>
                 </Grid>
 
-                <Grid item xs={6} alignItems="center" >
+                <Grid item xs={6} alignItems="center" style={{ height: '50vh' }} >
                     <Box justifyContent="center" alignItems="center">
                         <Box marginBottom='40px' >
-                            <Typography variant='h3'>Produto Ideal</Typography>
+                            <Typography variant='h4'>Produto Ideal</Typography>
                         </Box>
 
                         <Box className='alinhar' display='flex' marginBottom='5px'>
                             <Box marginRight='30px'>
-                                <Typography variant='h5'>1.</Typography>
+                                <Typography variant='h6'>1.</Typography>
                             </Box>
-                            <Typography variant='h5' style={{ fontWeight: 'bold' }}>Usuário</Typography>
+                            <Typography variant='h6' style={{ fontWeight: 'bold' }}>Usuário</Typography>
                         </Box>
-                        <Typography variant='h6'>Durante todo o desenvolvimento do projeto esse é o nosso</Typography>
-                        <Typography variant='h6'>principal foco: as pessoas que irão utilizar o produto.</Typography>
 
-                        <Box className='alinhar' display='flex' marginTop='30px' marginBottom='5px'>
-                            <Box marginRight='30px'>
-                                <Typography variant='h5'>2.</Typography>
-                            </Box>
-                            <Typography variant='h5' style={{ fontWeight: 'bold' }}>Negócio</Typography>
-                        </Box>
-                        <Typography variant='h6'>Não deixamos de pensar no modelo de negócio de</Typography>
-                        <Typography variant='h6'>quem está contratando pelo desenvolvimento do produto.</Typography>
+                        <Box paddingRight={20}>
+                            <Typography variant='h6'>Durante todo o desenvolvimento do projeto esse é o nosso principal foco: as pessoas que irão utilizar o produto.</Typography>
 
-                        <Box className='alinhar' display='flex' marginTop='30px' marginBottom='5px'>
-                            <Box marginRight='30px'>
-                                <Typography variant='h5'>3.</Typography>
+                            <Box className='alinhar' display='flex' marginTop='30px' marginBottom='5px'>
+                                <Box marginRight='30px'>
+                                    <Typography variant='h6'>2.</Typography>
+                                </Box>
+                                <Typography variant='h6' style={{ fontWeight: 'bold' }}>Negócio</Typography>
                             </Box>
-                            <Typography variant='h5' style={{ fontWeight: 'bold' }}>Estética</Typography>
+                            <Typography variant='h6'>Não deixamos de pensar no modelo de negócio de quem está contratando pelo desenvolvimento do produto.</Typography>
+
+                            <Box className='alinhar' display='flex' marginTop='30px' marginBottom='5px'>
+                                <Box marginRight='30px'>
+                                    <Typography variant='h6'>3.</Typography>
+                                </Box>
+                                <Typography variant='h6' style={{ fontWeight: 'bold' }}>Estética</Typography>
+                            </Box>
+                            <Typography variant='h6'>Ao mesmo tempo, priorizamos a estética do projeto. É de suma importância que o produto seja visualmente agradável.</Typography>
                         </Box>
-                        <Typography variant='h6'>Ao mesmo tempo, priorizamos a estética do projeto. É de</Typography>
-                        <Typography variant='h6'>suma importância que o produto seja visualmente agradável.</Typography>
                     </Box>
+
                 </Grid>
 
                 {/* PAGE 3 */}
+                <Grid item xs={6} alignItems="center" style={{ height: '40vh' }}>
+                    <Box paddingLeft={20} marginTop={10}>
+                        <Typography variant='h4'>Nosso Diferencial</Typography>
+                        <Typography variant='h6'>Nossa equipe é especializada em diagnosticar a sua necessidade e desenvolver a melhor solução para o seu negócio. Trabalhamos com grandes empresas até startups, sempre entregando aplicações web robustas para Sistemas Customizados.</Typography>
+                    </Box>
+
+                </Grid>
+
+                <Grid item xs={6} alignItems='center' style={{ height: '40vh' }}>
+                    <Box display='flex' justifyContent='center' alignItems='center' marginTop={10}>
+                        <img width="40%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="" />
+                    </Box>
+                </Grid>
+
+                {/* PAGE 4 */}
                 <Grid item xs={12} alignItems="center" style={{ height: '80vh' }}>
-                    <Box justifyContent="center" alignItems="center" margin={15}>
-                        <Box marginBottom={12}>
-                            <Typography variant='h3' align='center'>Tecnologias que utilizamos</Typography>
+                    <Box justifyContent="center" alignItems="center" margin={20}>
+                        <Box marginBottom={10}>
+                            <Typography variant='h4' align='center'>Tecnologias que utilizamos</Typography>
                         </Box>
                         <Box marginBottom={12} display='flex' justifyContent='space-between' alignItems='center'>
                             <img width="8%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="" />
@@ -128,8 +128,7 @@ export const Home = () => {
                             <img width="8%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="" />
                             <img width="8%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="" />
                         </Box>
-                        <Typography variant='h5' align='center'>A Nogran faz com que as tecnologias mais modernas e estáveis do mercado cheguem para seus clientes,</Typography>
-                        <Typography variant='h5' align='center'>implementando soluções atuais para problemas antigos.</Typography>
+                        <Typography variant='h6' align='center'>A Nogran faz com que as tecnologias mais modernas e estáveis do mercado cheguem para seus clientes, implementando soluções atuais para problemas antigos.</Typography>
                     </Box>
                 </Grid>
 
@@ -137,16 +136,16 @@ export const Home = () => {
                 <Grid item xs={12} alignItems="center">
                     <CardContent className='folder' style={{ height: '50vh' }} >
                         <Box margin={5} >
-                            <Typography align='center' variant='h2'>Gostamos de desafios!</Typography>
-                            <Typography align='center' variant='h4'>Portanto, seja qual for sua demanda,</Typography>
-                            <Typography align='center' variant='h4'>entre em contato e converse com</Typography>
-                            <Typography align='center' variant='h4'>nossa equipe.</Typography>
+                            <Typography align='center' variant='h3'>Gostamos de desafios!</Typography>
+                            <Typography align='center' variant='h5'>Portanto, seja qual for sua demanda,</Typography>
+                            <Typography align='center' variant='h5'>entre em contato e converse com</Typography>
+                            <Typography align='center' variant='h5'>nossa equipe.</Typography>
                         </Box>
 
                     </CardContent>
 
                     <Box margin={3} className='upper-line' display='flex' alignItems="center" justifyContent="center" >
-                        <Typography align="center" variant='h5'>Estamos à disposição para atendê-los.</Typography>
+                        <Typography align="center" variant='h6'>Estamos à disposição para atendê-los.</Typography>
                         <Link to='/contato' className='text-decorator-none'>
                             <Box marginLeft={4}>
                                 <StyledButton variant="contained" style={{ fontWeight: 'bold' }}>ENTRE EM CONTATO</StyledButton>
