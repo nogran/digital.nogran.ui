bd8c8167cc245683c54cf5193840cba6661bd4f3
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-02 09:42:11 -0300

    empresa update

--- src/pages/empresa/Empresa.tsx
+++ src/pages/empresa/Empresa.tsx
@@ -50,7 +50,7 @@ export const Empresa = () => {
                 <Grid item xs={6} style={{ height: '50vh' }} >
                     <Box marginLeft={20}>
                         <Typography align="left" variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}> DIGITAL NOGRAN</Typography>
-                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Somos uma empresa formada por consultores com vasta experiência nas áreas de tecnologia e desenvolvimento de software.</Typography>
+                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Somos uma empresa que tem como objetivo entregar soluções tecnológicas customizadas para o seu negócio. A empresa é formada por profissionais que viram a necessidade de trabalhar com atendimento exclusivo na criação de Sistemas Web.</Typography>
                         <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Nosso maior objetivo é entender as necessidades dos nossos clientes, transformando-as em valor agregado ao seu negócio. Entregamos soluções com qualidade e através de uma relação de parceria, transparência e comprometimento.</Typography>
                         <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Trabalhamos utilizando metodologias de desenvolvimento ágil, sempre percorrendo os prazos acordados. Nosso grande diferencial é o relacionamento com o cliente. Buscamos estabelecer um relacionamento estreito, uma parceria sincera e verdadeira. </Typography>
                     </Box>
@@ -63,8 +63,8 @@ export const Empresa = () => {
                             autoplay={{ delay: 3500 }}
                             pagination={true}>
 
-                            <SwiperSlide className="bgEmpresa"></SwiperSlide>
-                            <SwiperSlide className="bgEmpresa"></SwiperSlide>
+                            <SwiperSlide className="bgEmpresa1"></SwiperSlide>
+                            <SwiperSlide className="bgEmpresa2"></SwiperSlide>
                         </Swiper>
                     </Box>
                 </Grid>
@@ -74,23 +74,31 @@ export const Empresa = () => {
                 </Grid>
 
                 {/* PAGE 3 */}
-                <Grid item xs={12} style={{ height: '30vh' }}>
+                <Grid item xs={12} style={{ height: '30vh', marginTop:'20px' }}>
                     <Typography variant='h3' align="center">Olhar para o Futuro.</Typography>
-                    <Typography align="center" variant='h6' style={{ marginTop: 15 }}>Somos uma empresa formada por consultores com vasta experiência nas áreas de tecnologia e desenvolvimento de software.</Typography>
+                    <Box>
+                        <Typography variant='h6' align='center' style={{ marginLeft: '170px', marginRight: '170px', marginTop: '10px' }}>Somos uma empresa formada por profissionais com vasta experiência nas áreas de tecnologia, desenvolvimento de software e produção industrial. Isso possibilita a visão ao futuro onde desenvolvemos as soluções digitais específicas que serão as ferramentas de trabalho para possibilitar a criação de uma nova jornada para nossos clientes.</Typography>
+                    </Box>
                 </Grid>
 
                 {/* PAGE 4 */}
                 <Grid item xs={4} className="missao" style={{ height: '40vh' }}>
-                    <Typography variant='h5' align="center" style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Objetivo</Typography>
-                </Grid>
-
-
-                <Grid item xs={4} className="missao" style={{ height: '40vh' }}>
-                    <Typography variant='h5' align="center" style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Missão</Typography>
+                    <Box marginLeft='170px'>
+                        <Typography variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Objetivo</Typography>
+                        <Typography variant='h6' style={{ marginTop: '10px' }}>Somos uma empresa formada por profissionais com vasta experiência nas áreas de tecnologia.</Typography>
+                    </Box>
                 </Grid>
 
                 <Grid item xs={4} className="missao" style={{ height: '40vh' }}>
-                    <Typography variant='h5' align="center" style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Visão</Typography>
+                    <Box marginLeft='70px' marginRight='70px'>
+                        <Typography variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Missão</Typography>
+                        <Typography variant='h6' style={{ marginTop: '10px' }}>Somos uma empresa formada por profissionais com vasta experiência nas áreas de tecnologia.</Typography>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={4} className="missao" style={{ height: '40vh' }}>
+                    <Typography variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Visão</Typography>
+                    <Typography variant='h6' style={{ marginRight: '170px', marginTop: '10px' }}>Somos uma empresa formada por profissionais com vasta experiência nas áreas de tecnologia.</Typography>
                 </Grid>
 
                 <Grid item xs={12} className="missao" style={{ height: '10vh' }}>
