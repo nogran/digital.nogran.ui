3a684e2064634af175c96a554f2c6ae2e43ff17d
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-26 15:13:23 -0300

    create service

---
+++ src/services/Service.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'http://localhost:8080/'
+})
+
+export const busca = async (url:any, setDados:any, header:any)=>{
+    const resposta = await api.get(url,header)
+    setDados (resposta.data)
+}
+
+// ---------Busca sem token---------
+export const buscasemtoken = async (url:any, setDados:any)=>{
+    const resposta = await api.get(url)
+    setDados (resposta.data)
+}
+
+// export const buscaId = async (url:any, setDados:any, header:any)=>{
+//     const resposta = await api.get(url,header)
+//     setDados (resposta.data)
+// }
+
+// export const post = async (url: any, dados:any, setDados: any, header:any) => {
+//     const resposta = await api.post(url, dados, header)
+//     setDados(resposta.data)
+// }
+
+// ---------Post sem token---------
+export const postsemtoken = async (url: any, dados:any, setDados: any) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
